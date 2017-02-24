7e1016483e6815e18b87d4962d0f95cce8b50ba3
Rasmus Ulslev Pedersen <Rasmus.ulslev.pedersen@gmail.com>
2017-02-24 14:15:51 +0100

    Using TCP fields for rt setup (first mini test)

--- tipsi/tipsi.c
+++ tipsi/tipsi.c
@@ -6,22 +6,73 @@
 // tipsi: simulator for tcp etc. packets
 
 #include <stdio.h>
+#include "tipsi.h"
 
-int receive (char pload_[], int size)
-{
-  printf ("%d bytes received\n", size);
-  for (int i = 0; i < size; i++){
-      printf("0x%X ", pload_[i]);
-  }
-  printf("\n");
+// shared knowledge by sender and receiver
+tcprt_t tcprt 
+          = {token_rt : 0xedda,
+             sprt_rt  : 0xedda,
+             dprt_rt  : 0xedda,
+             seqn_rt  : 0xedda,
+             ackn_rt  : 0xedda};
+
+int sender_send(tcppacketrt_t tcppacketrt){
+  receiver_receive (tcppacketrt);    
   return 1;
 }
 
-int main (void)
-{
-  printf ("Tipsi, the world's smallest packet simulator!\n");
-  char pload[] = { 0xE, 0xD, 0xD, 0xA };
-  if(receive (pload, sizeof(pload)) == 1)
-      printf("Success!");
+int receiver_receive (tcppacketrt_t tcppacketrt){
+  int maxrtdelay = tcppacketrt.seqn / tcprt.token_rt;
+  
+  tcppacketrt_t tcppacketrt_recv 
+                  = {sprt : 0xedda,
+                     dprt : 0xedda,
+                     syn  : 1,
+                     ack  : 1}; 
+
+  if (maxrtdelay <= 10) // ok with 10 ms
+    tcppacketrt_recv.win = maxrtdelay;
+  else // cannot meet this requirement
+    tcppacketrt_recv.win = 0;
+
+  receiver_send(tcppacketrt_recv);
+  
+  return 1;
+}
+
+int receiver_send(tcppacketrt_t tcppacketrt){
+    sender_receive(tcppacketrt);
+    return 1;
+}
+
+int sender_receive(tcppacketrt_t tcppacketrt){
+    if (tcppacketrt.win == 10) // receiver ok with 10 ms
+      printf("Receiver ok with 10 ms max delay");
+    else
+      printf("Receiver cannot meet request for max delay\n");
+    
+    return 1;
+}
+
+int main (void){
+  printf ("Tipsi, the world's smallest real-time packet simulator!\n");
+  
+  // set it up with max 10 ms delay
+  tcppacketrt_t tcppacketrt_send 
+                  = {sprt : 0xedda,
+                     dprt : 0xedda,
+                     seqn : 10 * 0xedda,
+                     syn  : 1};
+                     
+  // ask receiver                  
+  sender_send(tcppacketrt_send);
+  
+  // send data 
+  // ...
+  
+  // wait max 10 ms for data 
+  // ...
+  
+
   return 0;
 }
---
+++ tipsi/tipsi.h
@@ -0,0 +1,56 @@
+// Copyright: 2017, CBS/DTU
+// Authors: Rasmus Ulslev Pedersen (rup.itm@cbs.dk)
+//          Martin Schoeberl (masha@dtu.dk)
+// License: Simplified BSD License
+//
+// tipsi: Simulator for tcp etc. packets
+
+
+
+// real time info
+typedef struct { 
+   unsigned int token_rt : 16;
+   unsigned int sprt_rt  : 16;
+   unsigned int dprt_rt  : 16;   
+   unsigned int seqn_rt  : 32;
+   unsigned int ackn_rt  : 32;
+} tcprt_t;
+
+// tcp header
+typedef struct { 
+   unsigned int sprt  : 16; // Source Port
+   unsigned int dprt  : 16; // Destination Port  
+   unsigned int seqn  : 32; // Sequence Number
+   unsigned int ackn  : 32; // Sequence Number
+   // ...
+   unsigned int syn   :  1;  // SYN
+   unsigned int ack   :  1;  // ACK
+   unsigned int win   : 16;  // Window
+   // ...
+} tcppacketrt_t;
+
+int receiver_receive (tcppacketrt_t tcppacketrt);
+int sender_send (tcppacketrt_t tcppacketrt);
+int sender_receive (tcppacketrt_t tcppacketrt);
+int receiver_send (tcppacketrt_t tcppacketrt);
+
+// TCP Header Format (see https://tools.ietf.org/html/rfc793, Figure 3)
+//  0                   1                   2                   3
+//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |          Source Port          |       Destination Port        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                        Sequence Number                        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                    Acknowledgment Number                      |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |  Data |           |U|A|P|R|S|F|                               |
+// | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+// |       |           |G|K|H|T|N|N|                               |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |           Checksum            |         Urgent Pointer        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                    Options                    |    Padding    |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                             data                              |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
