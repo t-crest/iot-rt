5587a24d93e56066e693fbb6ff9b2fef3407b824
Rasmus Ulslev Pedersen <rasmus.ulslev.pedersen@gmail.com>
2017-11-08 12:52:34 +0100

    buffer size in bytes

--- src/main/c/tpip.c
+++ src/main/c/tpip.c
@@ -1,21 +1,21 @@
 /*
   tpIP, the time-predictable TCP/IP stack.
 
-  Copyright: DTU
-  Authors: Martin Schoeberl
+  Copyright: DTU, CBS
+  Authors: Martin Schoeberl, Rasmus Ulslev Pedersen
   License: Simplified BSD
 */
 
 #include <stdio.h>
 
 #define MAX_BUF_NUM 4
-#define MAX_BUF_SIZE 1024
+#define MAX_BUF_SIZE 1024 // bytes in multiples of 4
 
 // Fixed, static buffers to enable WCET analysis
-static int buffer[MAX_BUF_NUM][MAX_BUF_SIZE];
+static int buffer[MAX_BUF_NUM][MAX_BUF_SIZE/4];
 
 int main() {
-  printf("Hello tpip!\n");
+  printf("Hello tpip world!\n");
 
   return 0;
 }
