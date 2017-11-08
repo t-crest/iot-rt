ccaf4c28a53ae48daa0dddb7fc2cc05da3409fdf
Martin Schoeberl <martin@jopdesign.com>
2017-11-08 12:28:33 +0100

    Start the C version of tpIP

---
+++ src/main/c/tpip.c
@@ -0,0 +1,16 @@
+/*
+  tpIP, the time-predictable TCP/IP stack.
+
+  Copyright: DTU
+  Authors: Martin Schoeberl
+  License: Simplified BSD
+*/
+#define MAX_BUF_NUM 4
+#define MAX_BUF_SIZE 1024
+
+// Fixed, static buffers to enable WCET analysis
+static int buffer[MAX_BUF_NUM][MAX_BUF_SIZE];
+
+int main() {
+  printf("Hello tpip!\n");
+}
