4ac5df67c05aeaacbae524f6277c20a564d972da
Martin Schoeberl <martin@jopdesign.com>
2017-11-11 16:36:27 +0100

    scala: use our RT api for the code example

--- src/main/scala/tpip/App.scala
+++ src/main/scala/tpip/App.scala
@@ -1,29 +1,37 @@
 package tpip
 
+import rtapi._
+
+class PeriodicApp(period: Int) extends RtThread(period) {
+
+  println("Hello from the real-time TCP/IP app")
+
+  val tpip = new Tpip()
+  var i = 0
+
+  def run(): Unit = {
+    while (true) {
+      tpip.run()
+      i += 1
+      if (i == 3) {
+        println("Send a ping")
+        val p = tpip.ll.txChannel.freePool.deq
+        p.buf(0) = 0xab.toByte
+        p.buf(1) = 0xcd.toByte
+        p.len = 2
+        tpip.ll.txChannel.queue.enq(p)
+      }
+      waitForNextPeriod()
+    }
+  }
+}
+
 /**
  * A simple example app for the TCP/IP stack
  */
 object App extends App {
 
-  println("Hello from the real-time TCP/IP app")
+  new PeriodicApp(1000)
   
-  val tpip = new Tpip()
-  
-  // This is our simulated periodic loop.
-  // It is not correct as we wait a constant time and not for the next period.
-  var i = 0
-  while (true) {
-//    println("ping")
-    Thread.sleep(1000)
-    tpip.run()
-    i += 1
-    if (i == -1) {
-      println("Send a ping")
-      val p = tpip.ll.txChannel.freePool.deq
-      p.buf(0) = 0xab.toByte
-      p.buf(1) = 0xcd.toByte
-      p.len = 2
-      tpip.ll.txChannel.queue.enq(p)
-    }
-  }
+  RtThread.startMission()
 }
