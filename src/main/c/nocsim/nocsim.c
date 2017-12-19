5f3e86468803e6e6143a514bd1f9f713acc1e0b6
Rasmus Ulslev Pedersen <rasmus.ulslev.pedersen@gmail.com>
2017-12-19 17:16:31 +0100

    nocsim: text on structuring it for the first 2x2 MVP

--- src/main/c/nocsim/nocsim.c
+++ src/main/c/nocsim/nocsim.c
@@ -34,3 +34,21 @@ int main(int argc, char *argv[])
    
    pthread_exit(NULL);
 }
+
+// Temp notes for a little design thinking //
+// For a simulation harness, we can make a "nocsim" harness that models the main components in
+// C structs, but are not meant to be run on the target (patmos). It has structs like
+// one-way-mem-noc struct, router structs, links structs, ni struct, tx mem struct, rx mem struct,
+// core struct, node (consists of core, rx, tx, ni) struct, tile (consists of node, router,
+// and links) struct.
+// Clocking: It is perhaps best to think in cycles as each cycle is a mini-transaction. Milli-second
+// based clocking is not going to work. It is fairly easy to use this clocking "scheme" in a
+// multithreaded simulator as the (nice) mesochronous clock property makes it possible
+// to read the current clock cycle, but care has to be taken to remember that another
+// router is perhaps already shifted into the next clock cycle. 
+// Ideas for hard simulations, hard calculations:
+//   1) Each core wants to read on each cycle (we must be able to *calculate* WCET here as the
+//        sum of routing, latency, TDM scheduling *and* local app code wcet (platin analysis of a
+//        given benchmark C code).
+//   2) Each core wants to to write on each cycle (same argument as above, we should be able to
+//        calculate it. 
