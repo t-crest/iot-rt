091559ca04e8da3da09a0ed271d2f66389b068e4
Rasmus Ulslev Pedersen <rasmus.ulslev.pedersen@gmail.com>
2018-01-22 20:11:39 +0100

    tpip: some skeleton files

---
+++ src/main/c/tpip/tpip.h
@@ -0,0 +1,19 @@
+/*
+  tpIP, the time-predictable TCP/IP stack.
+
+  Copyright: DTU, CBS
+  Authors: Martin Schoeberl, Rasmus Ulslev Pedersen
+  License: Simplified BSD
+*/
+#ifndef TPIP_H
+#define TPIP_H
+
+// access to HTTP
+
+// access to TCP
+
+// access to UDP
+
+// access to IP
+
+#endif
---
+++ src/main/c/tpip/udpclient.c
@@ -0,0 +1,10 @@
+/*
+  tpIP, the time-predictable TCP/IP stack.
+  udp client
+
+  Copyright: DTU, CBS
+  Authors: Rasmus Ulslev Pedersen, Martin Schoeberl
+  License: Simplified BSD
+*/
+
+// send udp packet to udp server
