128e9aa467718b691cadc868aa0a8ab53752445e
Martin Schoeberl <martin@jopdesign.com>
2018-02-08 11:18:19 +0100

    tpip: SLIP in polling mode

--- tpip/slip.c
+++ tpip/slip.c
@@ -12,34 +12,83 @@
 #define MAX 2000
 
 static int fd;
+static unsigned char rxbuf[MAX];
+static int is_esc = 0;
+static int cnt = 0;
+static int rxfull = 0;
 
-unsigned char buf[MAX];
-int is_esc = 0;
-int cnt = 0;
-
-
+static unsigned char txbuf[MAX];
+// TODO some counters and flags for TX
 
 int tpip_slip_init(char *str) {
 
-  fd = open(str, O_RDONLY);
-  // O_NDELAY returns 0 when no character available, maybe useful for the real polling thing
+#ifdef __patmos__
+  // no init needed
+#endif
+#ifndef __patmos__
+  // O_NDELAY returns 0 when no character available, useful for polling
+  fd = open(str, O_RDONLY | O_NDELAY);
   printf("SLIP open: %s %d\n", str, fd);
-
+#endif
   return 0;
 }
 
+
+int tpip_slip_getchar(char *cptr) {
+#ifdef __patmos__
+  TODO: UART reading
+#endif
+#ifndef __patmos__
+  return read(fd, cptr, 1);
+#endif
+}
+
+void tpip_slip_putchar(char c) {
+  // TODO send one character
+}
+
+// peridic stuff
+// TODO: sometimes a wrong end of packet detection. Worked in the non-polling mode :-()
+// TODO: should also contain the sending
 void tpip_slip_run() {
-  // peridic stuff
-  printf("hello\n");
+
+    unsigned char c;
+
+    for(int i=0; i<4; ++i) {
+      if(tpip_slip_getchar(&c) == 1) {   
+        if (is_esc) {
+          if (c == ESC_ESC) {
+            rxbuf[cnt++] = ESC;
+          } else if (c == ESC_END) {
+            rxbuf[cnt++] = END;
+          }
+          is_esc = 0;
+        } else if (c == ESC) {
+          is_esc = 1;
+        } else if (c == END) {
+          rxfull = 1;
+        } else {
+          rxbuf[cnt++] = c;
+        }
+        if (cnt == 2000) cnt = 0;
+      }
+    }
 }
 
 int tpip_slip_rxfull() {
-  // 
-  return 0;
+  return rxfull;
 }
 
 int tpip_slip_rxread(unsigned char buf[]) {
-  return 123;
+ 
+  int i;
+
+  for (i=0; i<cnt; ++i) {
+    buf[i] = rxbuf[i];
+  }
+  rxfull = 0;
+  cnt = 0;
+  return i;
 }
 
 int tpip_slip_txempty() {
@@ -48,53 +97,4 @@ int tpip_slip_txempty() {
 
 void tpip_slip_txwrite(unsigned char buf[], int len) {
 
-}
-
-// below is stuff that needs to be changed and other names.
-// Don't look at it now
-void tpip_print(unsigned char buf[], int len) {
-  
-  printf("One packet:\n");
-  for (int i=0; i<len; ++i) {
-    printf("%02x ", buf[i]);
-    if (i%4 == 3) printf("\n");
-  }
-  printf("\n\n");
-}
-
-
-void tpip_slip(char *dev) {
-
-  unsigned char c;
-
-  LL_INIT(dev);
-
-/*
-  for (;;) {
-    if (read(fd, &c, 1) == 1) {
-      printf("%c", c);
-    }
-  }
-*/
-for(;;) {
-  while(read(fd, &c, 1) == 1) {
-    
-    if (is_esc) {
-      if (c == ESC_ESC) {
-        buf[cnt++] = ESC;
-      } else if (c == ESC_END) {
-        buf[cnt++] = END;
-      }
-      is_esc = 0;
-    } else if (c == ESC) {
-      is_esc = 1;
-    } else if (c == END) {
-      tpip_print(buf, cnt);
-      cnt = 0;
-    } else {
-      buf[cnt++] = c;
-    }
-    if (cnt == 2000) cnt = 0;
-  }
-}
 }
--- tpip/test_slip.c
+++ tpip/test_slip.c
@@ -1,21 +1,41 @@
 /*
   Test of SLIP
 */
+
+#include <stdio.h>
+
 #include "config.h"
 
 void tpip_slip(char *);
 
+void tpip_print(unsigned char buf[], int len) {
+  
+  printf("One packet:\n");
+  for (int i=0; i<len; ++i) {
+    printf("%02x ", buf[i]);
+    if (i%4 == 3) printf("\n");
+  }
+  printf("\n\n");
+}
+
 int main(int argc, char *argv[]) {
 
-  if (argc != 2) return -1;
-  tpip_slip(argv[1]);
-  return 0;
+  // TODO: there should be constants in config.h
+  unsigned char buf[2000];
 
-  // this shall be called periodically
-  LL_RUN();
+  if (argc != 2) return -1;
+  LL_INIT(argv[1]);
+
+  // this shall be executed periodically
+  for (;;) {
+    LL_RUN();
+    if (LL_RXFULL()) {
+      int len = LL_RXREAD(buf);
+      tpip_print(buf, len);
+    }
+  }
 
   // just call all to check if everything compiles
-  unsigned char buf[100];
 
   int valid = LL_RXFULL();
   int len = LL_RXREAD(buf);
